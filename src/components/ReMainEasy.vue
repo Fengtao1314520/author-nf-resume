200b4477689e84b88a5826b73016146050e401e6
冯涛Nate <fengtao.1314520@163.com>
2022-08-22 20:55:30 +0800

    更新简历
    更新简历

--- src/components/ReMainEasy.vue
+++ src/components/ReMainEasy.vue
@@ -12,7 +12,23 @@
           </v-toolbar-title>
         </v-app-bar>
       </v-card-title>
-      <p class="ml-8 text-caption">{{ proinfo.desc }}</p>
+      <!--      
+        <p class="ml-8 text-caption">{{ proinfo.desc }}</p>
+       -->
+      <v-card flat class="ml-4">
+        <!--
+        <v-app-bar color="white" dense flat>
+          <v-app-bar-nav-icon disabled />
+          <v-toolbar-title class="text-h6 pl-0">个人简述:</v-toolbar-title>
+        </v-app-bar>
+        -->
+        <v-card-text class="pt-0">
+          <div v-for="(item, i) in profile" :key="i" >
+            <v-icon small class="mr-2">{{ item.icon }}</v-icon>
+            <span>{{ item.text }}</span>
+          </div>
+        </v-card-text>
+      </v-card>
       <v-divider />
       <v-card-text>
         <v-card flat>
@@ -59,20 +75,6 @@
             </v-row>
           </v-card-text>
         </v-card>
-        <!--
-        <v-card flat>
-          <v-app-bar color="white" dense flat>
-            <v-app-bar-nav-icon disabled />
-            <v-toolbar-title class="text-h6 pl-0">个人简述:</v-toolbar-title>
-          </v-app-bar>
-          <v-card-text class="pt-0">
-            <div v-for="(item, i) in profile" :key="i">
-              <v-icon small class="mr-2">{{ item.icon }}</v-icon>
-              <span>{{ item.text }}</span>
-            </div>
-          </v-card-text>
-        </v-card>
-        -->
         <v-divider />
         <v-card flat>
           <v-app-bar color="white" dense flat>
@@ -133,17 +135,17 @@
               <p
                 class="text-caption mb-2"
                 v-for="(two, k) in item.content.todo"
-                :key="k"
+                :key="k+'_k'"
               >
                 {{ two }}
               </p>
               <p class="text-subtitle-2 pt-2">项目业绩:</p>
               <p
                 class="text-caption mb-2"
-                v-for="(two, k) in item.content.done"
-                :key="k"
+                v-for="(three, p) in item.content.done"
+                :key="p"
               >
-                {{ two }}
+                {{ three }}
               </p>
             </v-alert>
           </v-card-text>
@@ -167,30 +169,32 @@ export default {
         tel: '17701503218',
         email: 'fengtao.1314520@163.com',
         resume: 'fengtao1314520.github.io/resume',
-        base: '常州',
-        desc: '超过10年自动化测试与开发经验，可以独立主导PC桌面、Web端、APP和基于协议的嵌入式软件的自动化测试的建设，拥有开发web网站和后端服务的经验。',
+        base: '江苏, 常州',
+        desc: '超过10年自动化测试与开发经验; 可以独立主导桌面程序、Web端、APP和基于协议的嵌入式软件、系统的自动化测试的开发工作，基于VUE.JS与Vuetify.js独立开发web网站的经验;以及基于C#开发后端服务、程序的经验。',
       },
       // 个人简介
       profile: [
+        /**
         {
           icon: 'mdi-dots-horizontal-circle-outline',
           text: '独立开发RoWarlock自动化测试工具&管理系统',
         },
+         **/
         {
           icon: 'mdi-dots-horizontal-circle-outline',
-          text: '针对不同软件具有独立开发完整自动化测试工具&框架的经验',
+          text: '针对不同平台的软件具有独立开发完整自动化测试工具、框架、脚本体系的经验',
         },
         {
           icon: 'mdi-dots-horizontal-circle-outline',
-          text: '能够基于Vue.js框架，独立开发前端网站，拥有开发后端系统的经验',
+          text: '熟悉和了解电子秤的嵌入式软件的功能测试和自动化测试工作',
         },
         {
           icon: 'mdi-dots-horizontal-circle-outline',
-          text: '熟悉和擅长使用多种开源、闭源性自动化测试工具',
+          text: '熟悉和擅长设计、编写测试计划、测试用例、测试执行等各位文档',
         },
         {
           icon: 'mdi-dots-horizontal-circle-outline',
-          text: '熟悉和擅长设计、编写、测试用例; 针对现有测试用例体系提出建议、改进',
+          text: '拥有基于Vue.js框架开发前端网站，基于C#开发后端系统的经验',
         },
         {
           icon: 'mdi-dots-horizontal-circle-outline',
@@ -208,11 +212,11 @@ export default {
             datetime: '2017-02~至今',
             desc: 'PC上位机包含Web版和桌面端以及云端，旨在PC端对嵌入式秤、PC秤进行数据管理，秤管理、各类自动化执行以及数据回传保存和生成报表以及复杂操作',
             todo: [
-              '1.负责测试MTStar,ScaleManager两款上位机软件的功能测试;',
-              '2.编写测试计划、测试用例和执行, 提交测试报告并汇总研发&测试最终文档;',
-              '3.开发MTStar软件的自动化测试脚本与执行工具，搭建上位机与秤内交互操作的测试环境;',
-              '4.负责PO&MO的售前、售后衔接工作; 针对不同区域的客户的需求进行售前、售后支持;',
-              '5.参与秤内自动化测试工具维护和改进, 提供新工具与新脚本架构的Demo与设计思路;',
+              '1.牵头MTStar,ScaleManager两款上位机软件的测试工作;',
+              '2.规划与制定测试计划; 安排测试任务; 编写测试报告并汇总研发&测试的最终释放文档;',
+              '3.设计并开发MTStar软件的自动化测试脚本与执行工具, 搭建上位机与电子秤的实际测试环境;',
+              '4.负责PO&MO的售前、售后衔接工作; 针对国内外不同区域的客户的需求提供售前、售后支持; 推动新业务、项目的落地;',
+              '5.参与秤内自动化测试工具维护和改进, 提供新工具与新脚本架构的设计思路与Demo;',
               '6.参与公司内部项目技术分享以及交流。',
             ],
           },
@@ -236,15 +240,20 @@ export default {
           text: '测试主管、自动化测试工程师',
           content: {
             company: '无锡文思海辉',
-            dept: '游戏基地、HP',
+            dept: 'GB1国内移动业务事业部, GB5欧美事业部',
             datetime: '2011-02~2015-08',
             desc: '国内最大的离岸服务外包公司; 游戏基地为江苏移动的移动端手机游戏APP商城; 展讯为展讯芯片兼容与APP测试; HP为HP scan自动化工具开发和自动化测试',
             todo: [
-              '1.负责管理游戏基地的团队、人员规划, 对测试工程师进行日常招聘、培训、任务分配、管理与考核;',
-              '2.负责游戏基地的设备资产管理, 测试用例, 各类报表的统计与分析;',
-              '3.负责游戏基地部门与各类学校的校企合作与兼职人员的绩效考核;',
-              '4.参与HP团队的自动化测试脚本开发工作, 参与与印度、美国两地的测试工程师研发沟通',
-              '5.负责HP scan的整体自动化测试架构, 领导、开发HP scan产品的自动化测试工作。',
+              /*
+               *  // todo  突出管理方面的优势
+               * */
+              '1.负责管理团队的架构、规划与人员规划; 对事业部内的测试工程师进行日常招聘、培训、任务分配、管理与考核;',
+              '2.负责制定事业部门内测试验收标注; 协同PM编写标书、各类质量报表、绩效报表、年表等提交客户;',
+              '3.负责管理事业部内的移动设备资产, 提供技术支持，例如各安卓机型版本升降级; 搭建WIFI测试环境和其他必要的测试环境;',
+              '4.负责事业部与各学校的校企合作、合作方公司、兼职人员的相关人员绩效考核; 推动各合作方的质量保障提升;',
+              '5.负责带领团队参与多事业部的合作与协作项目, 统筹与管理合作项目的各项事务,成功推动华为巴西电信项目成功实施; ',
+              '6.参与HP print项目的自动化测试脚本开发工作, 协调印度、美国两地的测试工程师研发沟通工作;',
+              '7.负责HP scan项目的整体自动化测试工具、脚本架构与设计工作; 领导开发HP scan产品的自动化测试工具和测试脚本, 推动HP scan项目组APP自动化测试成功落地。',
             ],
           },
         },
@@ -303,13 +312,13 @@ export default {
             ],
             todo: [
               '1.编写和执行上位机软件的功能测试用例;',
-              '2.负责上位机软件的释放与管理;',
-              '3.编写上位机软件的自动化测试脚本;',
-              '4.编写上位机软件的自动化测试工具。',
+              '2.负责上位机软件的释放与管理工作;',
+              '3.基于Selenium/Playwright编写上位机软件的自动化测试脚本;',
+              '4.使用.net C#编写上位机软件的自动化测试执行工具。',
             ],
             done: [
-              '1.保障了MTStar/Scale Manager的产品质量，不断提高交付的效率; 全年流出BUG率为0;',
-              '2.为客户提供多重的产品质量保障，为赢得客户订单提供了更多的产品优势, 包含多位客户因MTStar选择了我们的电子秤产品。',
+              '1.保障了MTStar/Scale Manager的产品质量，不断提高交付效率; 全年流出BUG率为0;',
+              '2.为客户提供多重的产品质量保障，为赢得客户订单提供了更多的产品优势。',
             ],
           },
         },
@@ -319,7 +328,7 @@ export default {
           datetime: '2019-01~2019-08',
           company: '梅特勒 托利多 (商衡事业部)',
           position: '测试开发工程师',
-          desc: 'Archone TestTool为梅特勒托利多的商用衡器的秤内自动化测试工具, 可以对电子秤进行基于协议的自动化测试。',
+          desc: 'Archone TestTool为梅特勒托利多的商用衡器的秤内自动化测试工具, 可以对电子秤进行基于ComOne协议的自动化测试。',
           content: {
             tech: [
               '功能测试',
@@ -333,13 +342,13 @@ export default {
               'C++',
             ],
             todo: [
-              '1.使用C++开发与改进Archone TestTool;',
-              '2.针对原始Archone TestTool支持的xml脚本提出新想法;',
+              '1.使用C++开发与改进Archone TestTool的部分功能;',
+              '2.针对原始Archone TestTool支持的xml脚本提出新脚本思路和设计理念;',
               '3.根据Archone平台的测试协议重新开发全流程工具与测试脚本, 使用Typescript与C#开发。',
             ],
             done: [
               '1.提升了测试工具的执行效率，提供更多的客户访问方法;',
-              '2.为更新的测试工具与平台提供多重可改进的选择。',
+              '2.为下一代的测试工具与平台提供可行性预研。',
             ],
           },
         },
@@ -374,12 +383,12 @@ export default {
             todo: [
               '1.参与HP打印机的自动化测试工作，基于HP Topcat自动化测试工具和VMPS执行工具进行自动化测试脚本编写和执行;',
               '2.负责HP扫描机的手机APP端自动化测试工作，基于Cucumber测试框架开发APP端自动化测试工具和测试脚本架构;',
-              '3.协调印度与美国的HP扫描机团队的测试工作，统一规划整个团队的自动化测试工作。',
+              '3.协调印度与美国的HP扫描机团队的测试工作，统一规划整个团队HP scan的自动化测试工作。',
             ],
             done: [
-              '1.从零开始搭建了HP扫描机团队的APP自动化测试体系;',
-              '2.为HP的打印机新产品提供了有力的质量保障;',
-              '3.为HP上海团队提供了Topcat工具改进的新方法;包含移动端和web端的自动化测试。',
+              '1.从零开始搭建了HP scan团队的APP自动化测试体系;',
+              '2.为HP的打印机多个新产品提供了有力的质量保障;',
+              '3.为HP上海团队提供了Topcat工具改进的新方法; 包含移动端和web端的自动化测试的技术预研工作。',
             ],
           },
         },
@@ -395,7 +404,7 @@ export default {
             todo: [
               '1.负责管理游戏基地项目组日常工作，包含行政管理和业务管理两部分;',
               '2.负责采购和管理游戏基地使用的手机以及硬件设备，包含超过300款不同类型的手机;',
-              '3.负责协同同PM下多个不同项目组的任务协调工作，包含手机借阅，人员流动和新项目的项目组建立。',
+              '3.参与测试各个游戏在不同机型上的执行表现，参照测试用例执行测试, 汇总结果并提交服务商报告。',
             ],
             done: [
               '1.保障了江苏移动的游戏基地内超过数万款的上线游戏的产品质量;',
